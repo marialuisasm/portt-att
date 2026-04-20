a8058667e92522d896dab996feed41256e69c61b
Maria Luisa <mluisasalesmendes@gmail.com>
2026-04-19 20:04:44 -0400

    Create carousel.js

---
+++ carousel.js
@@ -0,0 +1,14 @@
+new Glide('.glide', {
+            type: 'carousel',        
+            perView: 3,              
+            perMove: 1,              
+            autoplay: 3000,
+            hoverpause: true,
+            animationDuration: 800,
+            gap: 16,                 
+
+            breakpoints: {
+                1024: { perView: 2 },   // tablets → 2 itens
+                640:  { perView: 1 }    // celular → 1 item
+            }
+        }).mount();
